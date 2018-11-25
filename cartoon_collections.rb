998dc84b7a72a1fd0294bc774d63581f48237fc4
Cliff Jurgensen <jivefox.002@gmail.com>
2018-11-25 00:04:30 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,6 +10,7 @@ def summon_captain_planet(planeteers)
   planeteers.map do |planeteer|
     planeteer.capitalize << "!"
 end
+end
 
 def long_planeteer_calls# code an argument here
   # Your code here
